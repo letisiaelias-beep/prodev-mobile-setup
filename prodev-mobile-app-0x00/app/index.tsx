34815ed61b1f6d2983e002acff08605b753ad303
letisiaelias-beep <letisiaelias@gmail.com>
2025-11-02 20:18:21 +0300

    Create Your First Mobile App

---
+++ prodev-mobile-app-0x00/app/index.tsx
@@ -0,0 +1,16 @@
+// prodev-mobile-app-0x00/app/index.tsx
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Entry Index - First App Created</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  text: { fontSize: 18 },
+});
