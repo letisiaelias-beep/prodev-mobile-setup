33de1157102351cf5e79991016551c8a7a741abd
letisiaelias-beep <letisiaelias@gmail.com>
2025-11-02 20:48:58 +0300

    task 2 setup

---
+++ app-example/app-example/constants/Colors.tsx
@@ -0,0 +1,14 @@
+const Colors = {
+  light: {
+    text: '#000000',
+    background: '#ffffff',
+    tint: '#2f95dc',
+  },
+  dark: {
+    text: '#ffffff',
+    background: '#000000',
+    tint: '#2f95dc',
+  },
+};
+
+export default Colors;
